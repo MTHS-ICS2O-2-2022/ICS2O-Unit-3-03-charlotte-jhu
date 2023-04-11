21c3a5f87168b9f637fc80fd66b0b7cf8b28bc2a
charlotte-jhu <124389290+charlotte-jhu@users.noreply.github.com>
2023-04-11 23:51:42 +0000

    added code for webpage

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2023 Charlotte Jhu All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Charlotte Jhu
+// Created on: April 2023
 // This file contains the JS functions for index.html
+
+'use strict'
+
+/**
+ * This function calculates the volume of a sphere
+ */
+
+function myButtonClicked() {
+  // input
+  const radius = parseFloat(document.getElementById('radius').value)
+
+  // process
+  const volume = (4 / 3) * Math.PI * radius ** 3
+
+  // output
+  document.getElementById('volume').innerHTML = "Volume is " + volume.toFixed(2) + " cm³"
+}
